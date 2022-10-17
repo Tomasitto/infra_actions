9827396bdab785d328b8e64ae8c429867958c956
Tomasitto <ogurec98@yandex.ru>
2022-10-17 07:29:03 +0000

    try

---
+++ infra_project/infra_app1/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('/', views.index, name='index'),
+    path('/second_page/', views.second_page, name='page_2'),
+]
